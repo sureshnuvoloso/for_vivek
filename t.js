2a46a15852d4ba4445aa66b496b5db42bf03766b
suresh iyer <suresh.iyer@gmail.com>
2020-05-17 21:39:58 -0700

    misc clean up, values arent saved with this approach :(

--- t.js
+++ t.js
@@ -1,18 +1,19 @@
 
-
 function myFunction() {
-  var my_json = JSON.stringify(document.getElementById("originalForm").innerHTML);
+  //var my_json = JSON.stringify(document.getElementById("originalForm").innerHTML);
+  var my_json = document.getElementById("originalForm").innerHTML;
   console.log(my_json);
 
-  var filename = "hello.txt";
+  var filename = "stage1.txt";
 
   download(filename, my_json);
-  alert("The form was submitted");
+  alert("The form was submitted to a file", filename);
 }
 
 function download(filename, text) {
     var element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    //element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + text);
     element.setAttribute('download', filename);
 
     element.style.display = 'none';
@@ -39,35 +40,12 @@ window.onload = function(e) {
   document.body.appendChild(label);
   */
 
-  //var my_json = JSON.stringify(document.body);
-  //var my_json = JSON.stringify(document.getElementById("originalForm"));
-  /*
-  console.log(document);
-  console.log("\n");
-  console.log(document.getElementById("originalForm"));
-  console.log("\n");
-  console.log(document.body);
-  */
-
   var my_json = JSON.stringify(document.getElementById("originalForm").innerHTML);
   console.log(my_json);
+  var inner_html = document.getElementById("originalForm").innerHTML;
+  console.log(inner_html);
 
-  //var my_json = JSON.stringify(document.body);
-  //alert(my_json);
-  //alert(document);
-  //alert(document.getElementById("originalForm"));
   document.getElementById("originalForm").onsubmit = myFunction;
-  /*
-  document.getElementById("dwn-btn").addEventListener("click", function(){
-    // Generate download of hello.txt file with some content
-    //var text = document.getElementById("text-val").value;
-
-    var my_json = JSON.stringify(document.getElementById("originalForm").innerHTML);
-    var filename = "hello.txt";
-
-    download(filename, my_json);
-  }, false);
-  */
 };
 
 window.onbeforeunload = function(e) {
@@ -80,9 +58,4 @@ window.onbeforeunload = function(e) {
   download(filename, my_json);
   */
 }
-
-
-
-// Start file download.
-
 
